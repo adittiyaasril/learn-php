2457c15ba7422414c3266fda4c5bbc9e75e751b4
Adittiya Asril <adittiyaasril369@gmail.com>
2024-09-16 17:31:05 +0700

    Learn PHP

---
+++ pertemuan2/index.php
@@ -0,0 +1,37 @@
+<?php
+// Pertemuan 2  - PHP Dasar
+// Sintaks PHP
+
+// Standart Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan Sintaks PHP
+// Variable dan Tipe Data
+
+// Penggabungan String
+
+// $nama = "Adittiya";
+// $nama_belakang = "Asril";
+
+// echo $nama . " " . $nama_belakang
+
+// Assignment
+// =, +=, -=, /=, .= %=
+
+// Perbandingan
+// <, >, >=, <=, ==
+
+var_dump(1 > 5)
+
+// Identitas
+// ===, !==
+
+// Logika
+// &&, ||, !
+
+
+
+?>
+
---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,18 @@
+<?php
+// Pengkondisian
+// if else
+// if else else if
+// ternary
+// switch
+
+$x = 30;
+if($x < 20){
+    echo "Benar";
+}else if($x == 20){
+    echo "Bingo";
+}
+else {
+    echo "Salah";
+}
+
+?>
